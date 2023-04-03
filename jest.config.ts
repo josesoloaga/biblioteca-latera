1158384ceb0891acddccbd0cfe7833c13bde3da9
JoseSoloaga <josesoloagam@gmail.com>
2023-04-03 10:19:45 +0200

    wip: rebase

--- jest.config.ts
+++ jest.config.ts
@@ -1,15 +1,15 @@
 import { defaults } from 'jest-config'
 
 const config = {
-	verbose: true,
-	setupFiles: ['./jest.setup.ts'],
-	moduleFileExtensions: [...defaults.moduleFileExtensions, 'ts', 'tsx'],
-	transform: {
-		'^.+\\.tsx?$': 'ts-jest',
-	},
-	testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
-	testPathIgnorePatterns: ['/node_modules/', '/dist/'],
-	testEnvironment: 'node',
-}
+  verbose: true,
+  setupFiles: ["./jest.setup.ts"],
+  moduleFileExtensions: [...defaults.moduleFileExtensions, "ts", "tsx"],
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[tj]s?(x)"],
+  testPathIgnorePatterns: ["/node_modules/", "/dist/"],
+  testEnvironment: "node",
+};
 
 export default config
