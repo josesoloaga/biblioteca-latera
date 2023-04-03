5fb0a366e623d12f0585ca7c28db48cec603906f
nachorsanz <nachorsanz@gmail.com>
2023-04-03 11:43:56 +0200

    feat: resolve testing

--- src/ui/features/login/login-component.ui-jest.spec.tsx
+++ src/ui/features/login/login-component.ui-jest.spec.tsx
@@ -6,13 +6,26 @@ import Login from './login-component';
 import { Provider } from 'react-supabase';
 import { supabase } from '../../../api/client';
 import { render, screen } from '@testing-library/react';
+import { AppProvider } from '../../../config-adapters/context-provider';
+
+
+  jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+    useNavigate: jest.fn(),
+  }));
+
+
+
+
 
 describe('Login', () => {
   it('Render Login', () => {
     render(
+      <AppProvider>
       <Provider value={supabase}>
         <Login />
-      </Provider>,
+      </Provider>
+      </AppProvider>,
     );
     expect(screen.getByTestId('login-component')).toBeInTheDocument();
   });
