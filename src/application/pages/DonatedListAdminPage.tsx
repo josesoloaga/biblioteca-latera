1b44b50a1e286f17f8eb2f7cf2b11d2d717bbdc0
JoseSoloaga <josesoloagam@gmail.com>
2023-09-27 20:08:41 +0200

    feat: adjust donated list component render

--- src/application/pages/DonatedListAdminPage.tsx
+++ src/application/pages/DonatedListAdminPage.tsx
@@ -1,13 +1,5 @@
-import React, { useEffect, useState } from 'react';
 import DonatedListAdminComponent from '../ui/features/donatedListAdmin/donated-list-admin-component';
-import { getDonatedBooks } from '../../infra/api/get-books/get-donated-books';
-import { DonateBookForm } from '../../domain/models/donateBookForm';
 
 export const DonatedListAdminPage: React.FC = () => {
-
-
-
-  return (
-    <DonatedListAdminComponent />
-  );
+  return <DonatedListAdminComponent />;
 };
